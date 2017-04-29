67471c92c47ef2b9b81994c2d656dc204621e44e
Margaret Choi <margaret.j.li@gmail.com>
2017-04-29 02:53:45 -0700

    Complete guessing and images, need to fix winning alert and missed letter display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,36 @@
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "z", "y", "z"];
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "z", "y", "z", " "];
 
 var words = ["husky", "labrador", "maltese", "poodle", "doberman"];
 
-var guesses = [];
+var allGuesses = [];
+var storeGuesses = [];
+var wrongGuesses = []; 
+
+var userScore = 0;
+var userLives = 7;
+var missedWords = [];
 
 // Displays each letter as a button
-var printLetters = function() {
-	for (i = 0; i < letters.length; i++) {
-		document.getElementById("alphabet").innerHTML += "<button onclick='storeLetters()' id='guess'>" 
-		+ letters[i] + "</button>";
-	} // end for loop
-}; // end function
+// var printLetters = function() {
+// 	for (i = 0; i < letters.length; i++) {
+// 		var currentLetter = document.createElement("button");
+// 		document.getElementById("alphabet").appendChild(currentLetter);
+// 		currentLetter.id = letters[i];
+// 		currentLetter.innerHTML = letters[i];
+// 		currentLetter.addEventListener("click", storeLetters());
+// 	} // end for loop
+// }; // end function
 
-var storeLetters = function() {
-	var userGuess = document.getElementById("guess").innerHTML;
-	console.log(userGuess);
-	guesses.push(userGuess);
-}
+// var storeLetters = function() {
+// 	var userGuess = document.getElementById("a").innerHTML;
+// 	console.log(userGuess);
+// 	guesses.push(userGuess);
+// }
 
 // Selects a random word and replaces letters as blanks
+
+
+
 var gameStart = function() {
 
 	// Assigns gameWord to a random word
@@ -30,16 +42,132 @@ var gameStart = function() {
 	console.log(gameWord);
 	console.log(gameLetters);
 
-	var blankWord = [];
+	var blankLetters = [];
 
 	// Assigns each new blank letter to an array
 	for (i = 0; i < gameLetters.length; i++) {
 		// Replace gameLetter with _ in each index position
 		var gameBlanks = gameLetters[i].replace(gameLetters[i], "_");
-		blankWord.push(gameBlanks);
+		blankLetters.push(gameBlanks);
 	};
-	console.log(blankWord);
+	
+	console.log(blankLetters);
 
-	document.getElementById("word").innerHTML = blankWord.join(" ");
+	document.getElementById("word").innerHTML = blankLetters.join(" ");
 
-} 
+	var resetGuesses = function() {
+		document.getElementById("guesses").innerHTML = " ";
+		allGuesses.length = 0;
+		wrongGuesses.length = 0;
+		storeGuesses.length = 0;
+	}
+	
+	resetGuesses();
+
+	// Stores the users guess on keyup and compares it to the letters in the gameLetters
+	document.onkeyup = function(event) {
+		
+		// Stores the users key press
+		var userGuess = event.key;
+	
+		// Stores all user guesses
+		allGuesses.push(userGuess);
+		
+		var blankWord = blankLetters.join("");
+
+		if (gameLetters.includes(userGuess) === true) {
+			for (i = 0; i < gameLetters.length; i++) {
+				if (userGuess === gameLetters[i]) {
+					console.log(gameLetters[i]);
+					blankLetters.splice(i, 1, gameLetters[i]);
+					document.getElementById("word").innerHTML = blankLetters.join(" ");
+				}
+				// else if (userGuess !== gameLetters[i]) {
+				// 	wrongGuesses.push(userGuess);
+				// }
+			}
+		}
+
+		else if (gameLetters.includes(userGuess) === false) {
+			wrongGuesses.push(userGuess);
+			storeGuesses.push(wrongGuesses[1]);
+			console.log(storeGuesses);
+		};
+
+
+		console.log(blankLetters);
+		console.log(blankWord);
+		console.log(gameWord);
+		console.log(wrongGuesses);
+
+		// storeGuesses.push(wrongGuesses[1]);
+		// console.log(storeGuesses);
+		// wrongGuesses.length = 0;
+
+
+		var checkWord = function() {
+			if (blankWord === gameWord) {
+				alert("You win! Press play to guess again.");
+				userScore++;
+			}
+		}
+
+		checkWord();
+
+		document.getElementById("guesses").innerHTML = wrongGuesses + " ";
+
+		
+		if (storeGuesses.length > 0){
+			document.getElementById("dog-image").src = 
+			"assets/images/dog-2.jpg";
+		}
+		if (storeGuesses.length > 1){
+			document.getElementById("dog-image").src = 
+			"assets/images/dog-3.jpg";
+		}
+		if (storeGuesses.length > 2){
+			document.getElementById("dog-image").src = 
+			"assets/images/dog-4.jpg";
+		}
+		if (storeGuesses.length > 3){
+			document.getElementById("dog-image").src = 
+			"assets/images/dog-5.jpg";
+		}
+		if (storeGuesses.length > 4){
+			document.getElementById("dog-image").src = 
+			"assets/images/dog-6.jpg";
+		}
+		if (storeGuesses.length > 5){
+			document.getElementById("dog-image").src = 
+			"assets/images/dog-7.jpg";
+		}
+		if (storeGuesses.length > 6){
+			document.getElementById("dog-image").src = 
+			"assets/images/dog-8.jpg";
+		}
+		if (storeGuesses.length > 7){
+			document.getElementById("dog-image").src = 
+			"assets/images/dog-9.jpg";
+			alert("Oh no, you're caught! Try again.");
+			userLives--;
+		}
+
+
+		var newScore = function() { 
+			console.log(userScore);
+			console.log(userLives);
+			// document.getElementById("guesses").innerHTML = userScore + "<br>" + userLives;
+			document.getElementById("scores").innerHTML = "Score: " + userScore + "<br>" + "Lives: " + userLives;
+		}
+
+		newScore();
+		}
+
+
+	}
+
+
+
+
+
+
